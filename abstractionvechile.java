ac985b0b92890f6a4de0c1eea8b733c658f63b1c
amanpatial76-max <amanpatial76@gmail.com>
2026-05-17 12:56:05 +0530

    Add files via upload

---
+++ abstractionvechile.java
@@ -0,0 +1,22 @@
+public class abstractionvechile {
+    abstract void start();
+    void fuel Type() {
+System.out.println("Every vechile uses some fuel");
+    }
+}
+class car extentends Vechile{
+    void start() {
+        System.out.print("Cae starts with a key or self start"):
+    }
+}
+public class  main {
+    public static voud main(String[]args) {
+        Vechile v1 = new Car();
+        Vechile v2 = new Bike();
+        v1.start();
+        v1.fuelType();
+        v2.start();
+        v2.fuelType();
+        }
+    }
+    
